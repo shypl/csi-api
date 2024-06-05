2b8ea723003fe45eaca0d9349011f8dfa0979f08
Alexey Chuprin <a.chuprin@capjack.ru>
2024-06-05 23:55:46 +0300

    kotlin 2.0.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,6 @@
+plugins {
+	id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
 rootProject.name = "csi-api"
 
 include(
@@ -13,27 +16,3 @@ include(
 	"sandbox:generator",
 	"sandbox:app",
 )
-
-dependencyResolutionManagement {
-	versionCatalogs {
-		create("libs") {
-			library("shypl-tool-lang", "org.shypl.tool:tool-lang:1.0.0-SNAPSHOT")
-			library("shypl-tool-utils", "org.shypl.tool:tool-utils:1.0.0-SNAPSHOT")
-			library("shypl-tool-logging", "org.shypl.tool:tool-logging:1.0.0-SNAPSHOT")
-			library("shypl-tool-io", "org.shypl.tool:tool-io:1.0.0-SNAPSHOT")
-			library("shypl-tool-biser", "org.shypl.tool:tool-biser:1.0.0-SNAPSHOT")
-			
-			version("csi-core", "1.0.0-SNAPSHOT")
-			library("shypl-csi-core-common", "org.shypl.csi", "csi-core-common").versionRef("csi-core")
-			library("shypl-csi-core-client", "org.shypl.csi", "csi-core-client").versionRef("csi-core")
-			library("shypl-csi-core-server", "org.shypl.csi", "csi-core-server").versionRef("csi-core")
-			
-			version("csi-transport", "1.0.0-SNAPSHOT")
-			library("shypl-csi-transport-common", "org.shypl.csi", "csi-transport-common").versionRef("csi-transport")
-			library("shypl-csi-transport-client", "org.shypl.csi", "csi-transport-client").versionRef("csi-transport")
-			library("shypl-csi-transport-server", "org.shypl.csi", "csi-transport-server").versionRef("csi-transport")
-			
-			library("coroutines", "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
-		}
-	}
-}
