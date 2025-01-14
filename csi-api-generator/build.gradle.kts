f8b4ee413b5e0c76c1aceba9ae94ae2cc2beee8e
Alexey Chuprin <a.chuprin@capjack.ru>
2025-01-14 19:17:00 +0300

    kotlin 2.1.0 gradle 8.12

--- csi-api-generator/build.gradle.kts
+++ csi-api-generator/build.gradle.kts
@@ -9,7 +9,7 @@ dependencies {
 	implementation(libs.shypl.tool.utils)
 	implementation(kotlin("reflect"))
 	implementation("org.reflections:reflections:0.10.2")
-	implementation("ch.qos.logback:logback-classic:1.5.6")
-	implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.17.2")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.2")
+	implementation("ch.qos.logback:logback-classic:1.5.16")
+	implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.18.2")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.18.2")
 }
