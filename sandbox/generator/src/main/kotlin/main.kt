1fb8450a98c45da0e84c11da7f409da44745fe15
Alexey Chuprin <a.chuprin@capjack.ru>
2025-04-28 12:19:09 +0300

    Develop

--- sandbox/generator/src/main/kotlin/main.kt
+++ sandbox/generator/src/main/kotlin/main.kt
@@ -1,16 +1,16 @@
 import org.shypl.csi.api.generator.generateCsiApi
 
 fun main() {
-	generateCsiApi("sandbox.api", "sandbox/api/model.yml") {
+	generateCsiApi("sandbox.api", "sandbox/api/csi.yml") {
 		
 		serverKotlin("sandbox/api/server/src/main/kotlin")
 		clientKotlin("sandbox/api/client/src/main/kotlin")
 		
 		clientTypescript(
-			"/Users/shnyaka/workspace/projects/shypl/cocos/cocos-lib/src",
+			"sandbox/api/client/src/main/typescript",
 			"sandbox",
-			"test-csi-api",
-			"main"
+			"app.server.generated",
+			"lib"
 		)
 	}
 }
