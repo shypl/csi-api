1ed950b564539e9593ea0dc1292c35a54aca9bd9
Alexey Chuprin <a.chuprin@capjack.ru>
2026-01-22 18:17:46 +0300

    Update dependencies

--- csi-api-generator/build.gradle.kts
+++ csi-api-generator/build.gradle.kts
@@ -8,8 +8,8 @@ dependencies {
 	implementation(libs.shypl.tool.lang)
 	implementation(libs.shypl.tool.utils)
 	implementation(kotlin("reflect"))
-	implementation("org.reflections:reflections:0.10.2")
-	implementation("ch.qos.logback:logback-classic:1.5.16")
-	implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.18.2")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.18.2")
+	implementation(libs.reflections)
+	implementation(libs.logback)
+	implementation(libs.jackson.module.kotlin)
+	implementation(libs.jackson.dataformat.yaml)
 }
